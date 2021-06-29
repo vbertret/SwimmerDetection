12f07a61eeb48975e5f29d7c12f36cff398a31ac
Bertret Victor <victor.bertret@insa-rennes.fr>
2021-06-29 14:03:42 +0200

    New class for Gaussian Mixture

---
+++ src/gaussian_mixture.py
@@ -0,0 +1,179 @@
+import maxflow
+import cv2.cv2 as cv
+import numpy as np
+from sklearn.mixture import GaussianMixture
+import pandas as pd
+from src.preprocessing.water_surface_detection import surface_detection
+import pickle
+import time
+from skimage.util import img_as_ubyte
+from skimage import exposure
+from src.preprocessing.bb_tools import union, intersection
+
+class GaussianMixtureBB():
+
+    def __init__(self, filename=None, margin=100, threshold=0.5,  detect_surface=True, adjust_pt1=15, adjust_pt2=15, use_time=True):
+
+        if filename is None:
+            self.model = RandomForestClassifier(n_estimators=n_estimators)
+        else:
+            self.model = pickle.load(open(filename, 'rb'))
+
+        # Set the margin and the threshold
+        self.margin = margin
+        self.threshold = threshold
+
+        # Set the detect surface
+        self.detect_surface = detect_surface
+        if self.detect_surface:
+            self.adjust_pt1 = adjust_pt1
+            self.adjust_pt2 = adjust_pt2
+
+        # Set the time approach
+        self.use_time = use_time
+
+    def set_params(self, **params):
+        """
+        Set the parameters of the estimator
+
+        Parameters
+        ----------
+        **params : dict
+            Estimator parameters
+        """
+        if 'margin' in params:
+            self.margin = params.get('margin')
+
+        if 'detect_surface' in params:
+            self.detect_surface = params.get('detect_surface')
+
+        if 'threshold' in params:
+            self.threshold = params.get('threshold')
+
+        if 'adjust_pt1' in params:
+            self.adjust_pt1 = params.get('adjust_pt1')
+
+        if 'adjust_pt2' in params:
+            self.adjust_pt2 = params.get('adjust_pt2')
+
+        if 'use_time' in params:
+            self.use_time = params.get('use_time')
+
+    def predict(self, filename_img, debug=False, a=0, b=0, precBB=[]):
+        """
+        Prediction of the Random Forest
+
+        This method makes the prediction of the bounding box for one image
+
+        Parameters
+        -----------
+        filename_img : str
+            the name of the image to use for the prediction
+        debug : boolean
+            If true, the method shows different step of the algorithm
+        a : float
+            if different from 0, it's the slope of the surface line ( default is 0 )
+        b : float
+            if different from 0, it's the intercept of the surface line ( default is 0 )
+        precBB : [x, y, w, h] list
+            the coordinates of the bounding box found at the previous frame ( default is [] )
+
+        Returns
+        -------
+        best_rectangle : [x, y, w, h] list
+            the coordinates of the bounding box
+        """
+        # Loading image
+        img = cv.imread(filename_img, cv.IMREAD_COLOR)
+
+        if debug:
+            cv.imshow("image", img)
+            cv.waitKey(1)
+
+        if len(precBB)!=0:
+            x_prec, y_prec, w_prec, h_prec = precBB
+
+            x_prec = max(x_prec - self.margin, 0)
+            y_prec = max(y_prec - self.margin, 0)
+
+            w_prec = min(w_prec + 2*self.margin, 640 - x_prec)
+            h_prec = min(h_prec + 2*self.margin, 480 - y_prec)
+
+            img = img[y_prec:(y_prec + h_prec), x_prec:(x_prec + w_prec), :]
+
+        # Creation dataframe
+        img2 = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        img2 = img2.reshape(-1, 3)
+
+        df = pd.DataFrame()
+        df['ColourCode(H)'] = img2[:, 0]
+        df['ColourCode(S)'] = img2[:, 1]
+        df['ColourCode(V)'] = img2[:, 2]
+
+        # Compute prediction
+        prediction_gm = self.model.predict_proba(df)[:, 1]
+
+        # Building the mask
+        if len(precBB)!=0:
+            score_img = prediction_gm.reshape((h_prec, w_prec))
+        else:
+            score_img = prediction_gm.reshape((480, 640))
+        mask = np.array((score_img > self.threshold) * 255, dtype=np.uint8)
+
+        if a!=0 and b!=0:
+
+            if len(precBB)!=0:
+                pt1 = (0, int(a * x_prec + b) - y_prec)
+                pt2 = (w_prec, int(a * (x_prec + w_prec) + b) - y_prec)
+                pt3 = (w_prec, 0)
+                pt4 = (0, 0)
+            else:
+                pt1 = (0, int(a*0+b))
+                pt2 = (640, int(a*640+b))
+                pt3 = (640, 0)
+                pt4 = (0, 0)
+
+            polygon = np.array([pt1, pt2, pt3, pt4])
+            mask = cv.fillPoly(mask, [polygon], 0)
+
+        if debug:
+            cv.imshow('mask', mask)
+            cv.waitKey(1)
+
+        # Find contours
+        contours, hierarchy = cv.findContours(mask, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[-2:]
+
+        # Building the rectangle (only one)
+        rects = []
+        for cnt in contours:
+            x, y, w, h = cv.boundingRect(cnt)
+            if cv.contourArea(cnt) > 800:
+                find = False
+                for i, (x_r, y_r, w_r, h_r) in enumerate(rects):
+                    x_in, y_in, w_in, h_in = intersection([x_r - 20, y_r, w_r + 40, h_r], [x - 20, y, w + 40, h])
+                    if w_in != 0 and h_in != 0:
+                        new_rect = union([x_r, y_r, w_r, h_r], [x, y, w, h])
+                        rects[i] = new_rect
+                        find = True
+                if not find:
+                    rects.append([x, y, w, h])
+
+        best_rectangle = []
+        if len(rects) != 0:
+            x, y, w, h = max(rects, key=lambda p: p[1])
+            best_rectangle = [x, y, w, h]
+
+            if len(precBB)!=0:
+                best_rectangle[0] = best_rectangle[0] + x_prec
+                best_rectangle[1] = best_rectangle[1] + y_prec
+
+            cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        if debug:
+            cv.imshow('bounding box', img)
+            cv.waitKey(0)
+            cv.destroyAllWindows()
+
+        return best_rectangle
+
+
